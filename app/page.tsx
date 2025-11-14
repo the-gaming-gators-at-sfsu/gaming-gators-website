4f5c8d7ba8a84c8fe56187a379a0fd5051c44c19
devalto <51037255+dev-alto@users.noreply.github.com>
2025-11-14 03:06:25 -0800

    feat: add `IntroFade`

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
+import Image from "next/image"
+
+import { IntroFade } from "./components/IntroFade"
+
 export default function Home() {
   return (
     <main>
+      <IntroFade />
 
       {/* hero */}
       <div className="flex w-full h-screen">
         
-        <div className="w-full h-full">
-          <div className="absolute -z-10 w-full h-full bg-linear-to-b from-transparent via-transparent to-[#dc90f7]/50" />
-          <video className="absolute -z-20 w-full h-full object-cover" src="/hero_backdrop.webm" loop autoPlay muted></video>
+        <div className="w-full h-full flex items-center justify-center">
+          <div className="absolute -z-20 w-full h-full bg-linear-to-b from-transparent via-transparent to-[#dc90f7]/50" />
+          <video className="absolute -z-30 w-full h-full object-cover" src="/hero_backdrop.webm" loop autoPlay muted></video>
+          {/* <Image className="absolute w-[30%] aspect-square object-cover opacity-5" src="/bits/gaming_gators_logomark.webp" alt="" width={360} height={360} /> */}
         </div>
 
         <div>
@@ -23,5 +29,5 @@ export default function Home() {
         allowfullscreen>
       </iframe> */}
     </main>
-  );
+  )
 }
