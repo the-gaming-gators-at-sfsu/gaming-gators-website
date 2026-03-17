f3f075a577f3aa42891b48c2a44e41dbb71a4628
devalto <51037255+dev-alto@users.noreply.github.com>
2026-03-16 19:55:37 -0700

    feat: added section titles and improved spacing

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -2,10 +2,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import { teams } from '@/dispositions/teams'
+import { FaDiscord } from 'react-icons/fa'
 
 export default function Teams() {
     return (
-        <main className="">
+        <main className="flex flex-col items-center">
 
             {/* hero */}
             <div className="relative w-full h-[calc(26vw+72px)] min-h-64 flex items-center justify-center lg:justify-center border-y border-white/40 pt-[72px]">
@@ -24,29 +25,58 @@ export default function Teams() {
                 </div>
             </div>
 
-            {/* TODO: should ensure visual symmetry with the navbar teams section */}
-            <div className="w-full px-[10%] py-16 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center gap-8 xl:gap-12">
-            {Object.entries(teams).map(([teamName]) => {
-                const team = teams[teamName]
-                return (
-                    <Link
-                        className="relative max-h-48 2xl:max-h-64 aspect-video group"
-                        href={`/teams/${teamName}`}
-                        key={teamName}
-                    >
-                        {/* underglow blur effect */}
-                        <Image className="absolute w-full h-full object-cover group-hover:blur-md group-active:blur-xs opacity-0 group-hover:opacity-100 rounded-xl transition-all duration-200 group-active:duration-100" src={team.backdrop} alt="" width={1920} height={1080} />
+            <div className="relative w-full px-[15%] py-16">
 
-                        {/* main card body */}
-                        <div className="relative w-full h-full flex flex-col items-center justify-center rounded-xl overflow-hidden">
-                            <Image className="z-30 h-[50%] drop-shadow-2xl object-contain" src={team.logo} alt={teamName + " game logo"} width={1000} height={1000} />
-                            <div className="z-20 absolute w-full h-full bg-linear-to-b from-transparent to-black/85" />
-                            <Image className="z-10 absolute w-full h-full object-cover opacity-90 group-hover:blur-xs border-2 border-white/50 transition-all duration-500 rounded-xl" src={team.backdrop} alt={teamName} width={1920} height={1080} />
+                <Image className="-z-10 absolute left-0 top-0 w-full h-full object-cover opacity-3" src={"/hex_backdrop.webp"} alt="" width={1920} height={1080} />
+    
+                <div className="flex flex-col items-center gap-12">
+
+                    <div className="space-y-8">
+                        <div className="text-center space-y-2">
+                            <h1 className="text-5xl font-semibold">Our Lineup</h1>
+                            <p className="text-neutral-400">Select a card to view a team&apos;s active roster.</p>
                         </div>
-                    </Link>
-                )
-            })}
+
+                        {/* TODO: should ensure visual symmetry with the navbar teams section */}
+                        <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center gap-8 xl:gap-12">
+                        {Object.entries(teams).map(([teamName, team]) => {
+                            return (
+                                <Link
+                                    className="relative max-h-48 2xl:max-h-64 aspect-video group"
+                                    href={`/teams/${teamName}`}
+                                    key={teamName}
+                                >
+                                    {/* underglow blur effect */}
+                                    <Image className="absolute w-full h-full object-cover group-hover:blur-md group-active:blur-xs opacity-0 group-hover:opacity-100 rounded-xl transition-all duration-200 group-active:duration-100" src={team.backdrop} alt="" width={1920} height={1080} />
+
+                                    {/* main card body */}
+                                    <div className="relative w-full h-full flex flex-col items-center justify-center rounded-xl overflow-hidden">
+                                        <Image className="z-30 h-[50%] drop-shadow-2xl object-contain" src={team.logo} alt={teamName + " game logo"} width={1000} height={1000} />
+                                        <div className="z-20 absolute w-full h-full bg-linear-to-b from-transparent to-black/85" />
+                                        <Image className="z-10 absolute w-full h-full object-cover opacity-90 group-hover:blur-xs border-2 border-white/50 transition-all duration-500 rounded-xl" src={team.backdrop} alt={teamName} width={1920} height={1080} />
+                                    </div>
+                                </Link>
+                            )
+                        })}
+                        </div>
+                    </div>
+
+                    <div className="text-center space-y-2">
+                        <h1 className="text-5xl font-semibold text-purple-300">Joining a Team</h1>
+                        <p className="text-neutral-400">Looking to join us? Show your interest on our Discord.</p>
+                        <Link
+                            className="flex flex-row items-center justify-center gap-2 font-semibold hover:bg-blue-400/30 active:bg-transparent px-2 py-2 rounded-lg transition-colors"
+                            href="https://discord.gg/dpyyQfcphu"
+                            target="_blank"
+                        >
+                            <FaDiscord size={32} />
+                        </Link>
+                    </div>
+                </div>
+
             </div>
+
+            
         </main>
     )
 }
